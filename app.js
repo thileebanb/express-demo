e9c01487b3b7d12504f1d5d1523c8a6a422a54ba
Thileeban <thileebanb@wowtruck.in>
2021-03-28 12:38:44 +0530

    add: handle put request

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const Joi = require("joi");
+
 const app = express();
 
 app.use(express.json());
@@ -19,12 +21,17 @@ app.get("/api/:id", (req, res) => {
 });
 
 app.post("/api", (req, res) => {
-  if (!req.body.name || req.body.name.length < 3) {
-    res
-      .status(400)
-      .send("Name is required and should be minimum 3 characters.");
-    return;
-  }
+  // Joi Validation
+  const { error } = validateMoi(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Manual Validation
+  // if (!req.body.name || req.body.name.length < 3) {
+  //   res
+  //     .status(400)
+  //     .send("Name is required and should be minimum 3 characters.");
+  //   return;
+  // }
 
   const moi = {
     id: mois.length + 1,
@@ -33,6 +40,23 @@ app.post("/api", (req, res) => {
   mois.push(moi);
   res.send(moi);
 });
+
+app.put("/api:id", (req, res) => {
+  const id = req.params.id;
+  const moi = mois.find((m) => m.id === parseInt(id));
+  if (!moi) return res.status(404).send("Record not found");
+
+  const { error } = validateMoi(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  moi.name = req.body.name;
+  res.status(200).send(moi);
+});
+
+function validateMoi(moi) {
+  const schema = Joi.object({ name: Joi.string().min(3).required() });
+  return schema.validate(moi);
+}
 
 const port = process.env.PORT || 3000;
 
