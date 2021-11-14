f504d1b37d9fccbb900192132699edfb01b78880
Thileeban <thileebanb@wowtruck.in>
2021-11-14 14:50:22 +0530

    Connect with DB

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const debug = require("debug")("app:startup");
+const debugStartup = require("debug")("app:startup");
+const { connect, model } = require("mongoose");
+const config = require("config");
+const debugDb = require("debug")("app:db");
 
 const home = require("./routes/home");
 const api = require("./routes/api");
@@ -24,9 +27,42 @@ app.set("views", "./views"); // Optional
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  debug("Morgan enabled");
+  debugStartup("Morgan enabled");
   // console.log("Morgan enabled...");
 }
+
+async function connectDB() {
+  try {
+    await connect(config.dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    debugDb("Connected to MongoDB...");
+  } catch (error) {
+    debugDb("Could not connect to MongoDB...", error);
+  }
+}
+
+connectDB();
+
+const Moi = model("Moi", {
+  name: { type: String, required: true, minlength: 3 },
+});
+
+async function createMoi() {
+  const moi = new Moi({
+    name: "bala",
+  });
+
+  try {
+    const result = await moi.save();
+    console.log(result);
+  } catch (error) {
+    console.log(error.errors);
+  }
+}
+
+createMoi();
 
 const port = process.env.PORT || 3000;
 
